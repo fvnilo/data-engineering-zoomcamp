58f0929767f4f08efe76369c9c6978d2190ae29f
Ny Fanilo Andrianjafy <ny.fanilo@gmail.com>
2024-12-06 15:36:24 -0500

    02 - Use Importable Operator To Upload To GCS

--- 02-workflow-orchestration/01-airflow/dags/data_ingestion_gcs.py
+++ 02-workflow-orchestration/01-airflow/dags/data_ingestion_gcs.py
@@ -6,41 +6,17 @@ from airflow.utils.dates import days_ago
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 
-from google.cloud import storage
 from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateExternalTableOperator
-import pyarrow.csv as pv
-import pyarrow.parquet as pq
+from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesystemToGCSOperator
 
 PROJECT_ID = os.environ.get("GCP_PROJECT_ID")
 BUCKET = os.environ.get("GCP_GCS_BUCKET")
 BIGQUERY_DATASET = os.environ.get("GCP_BIGQUERY_DATASET")
 
-dataset_file = "yellow_tripdata_2021-01.parquet"
-dataset_url = f"https://d37ci6vzurychx.cloudfront.net/trip-data/{dataset_file}"
-
 path_to_local_home = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
 
-# NOTE: takes 20 mins, at an upload speed of 800kbps. Faster if your internet has a better upload speed
-def upload_to_gcs(bucket, object_name, local_file):
-    """
-    Ref: https://cloud.google.com/storage/docs/uploading-objects#storage-upload-object-python
-    :param bucket: GCS bucket name
-    :param object_name: target path & file-name
-    :param local_file: source path & file-name
-    :return:
-    """
-    # WORKAROUND to prevent timeout for files > 6 MB on 800 kbps upload speed.
-    # (Ref: https://github.com/googleapis/python-storage/issues/74)
-    storage.blob._MAX_MULTIPART_SIZE = 5 * 1024 * 1024  # 5 MB
-    storage.blob._DEFAULT_CHUNKSIZE = 5 * 1024 * 1024  # 5 MB
-    # End of Workaround
-
-    client = storage.Client()
-    bucket = client.bucket(bucket)
-
-    blob = bucket.blob(object_name)
-    blob.upload_from_filename(local_file)
-
+dataset_file = "yellow_tripdata_2021-01.parquet"
+dataset_url = f"https://d37ci6vzurychx.cloudfront.net/trip-data/{dataset_file}"
 
 default_args = {
     "owner": "airflow",
@@ -58,21 +34,16 @@ with DAG(
     max_active_runs=1,
     tags=['dtc-de'],
 ) as dag:
-
     download_dataset_task = BashOperator(
         task_id="download_dataset_task",
         bash_command=f"curl -sSL {dataset_url} > {path_to_local_home}/{dataset_file}"
     )
 
-    # TODO: Homework - research and try XCOM to communicate output values between 2 tasks/operators
-    local_to_gcs_task = PythonOperator(
-        task_id="local_to_gcs_task",
-        python_callable=upload_to_gcs,
-        op_kwargs={
-            "bucket": BUCKET,
-            "object_name": f"raw/{dataset_file}",
-            "local_file": f"{path_to_local_home}/{dataset_file}",
-        },
+    local_to_gcs_task = LocalFilesystemToGCSOperator(
+        task_id="upload_file",
+        src=f"{path_to_local_home}/{dataset_file}",
+        dst=f"raw/{dataset_file}",
+        bucket=BUCKET,
     )
 
     bigquery_external_table_task = BigQueryCreateExternalTableOperator(
