bc5c05e97e99f17a56c453b5aa8843648c0d8c48
Ny Fanilo Andrianjafy <ny.fanilo@gmail.com>
2025-01-06 11:25:28 -0500

    04 - Do Some Formatting

--- 04-analytics-engineering/models/core/fact_trips.sql
+++ 04-analytics-engineering/models/core/fact_trips.sql
@@ -5,13 +5,11 @@
 }}
 
 with green_tripdata as (
-    select *, 
-        'Green' as service_type
+    select *, 'Green' as service_type
     from {{ ref('stg_green_tripdata') }}
 ), 
 yellow_tripdata as (
-    select *, 
-        'Yellow' as service_type
+    select *, 'Yellow' as service_type
     from {{ ref('stg_yellow_tripdata') }}
 ), 
 trips_unioned as (
